1ec6c9cef111427b410d43b4f9096f2daaa3e90e
UT000650 <liujuna@uniontech.com>
2021-08-21 14:34:12 +0800

    fix: UT003
     UT003
    
    Log: UT003

--- deepin-devicemanager-server/src/LoadInfo/ThreadPool.cpp
+++ deepin-devicemanager-server/src/LoadInfo/ThreadPool.cpp
@@ -6,7 +6,6 @@
 #include <QProcess>
 #include <QDir>
 #include <QDebug>
-#include <QDateTime>
 
 ThreadPool::ThreadPool(QObject *parent)
     : QThreadPool(parent)
@@ -19,12 +18,6 @@ ThreadPool::ThreadPool(QObject *parent)
 
 void ThreadPool::loadDeviceInfo()
 {
-    // 在线程池执行之前，先用单线程获取hwinfo信息，因为在线程池里面执行hwinfo命令，可能出现死机的问题
-    qint64 begin = QDateTime::currentMSecsSinceEpoch();
-    runHwinfoCmd();
-    qint64 end = QDateTime::currentMSecsSinceEpoch();
-    qInfo() << "loadDeviceInfo*******************************" << (end - begin) / 1000;
-
     // 根据m_ListCmd生成所有设备信息
     QObjectCleanupHandler *cleaner = new QObjectCleanupHandler;
     cleaner->setParent(this);
@@ -39,12 +32,6 @@ void ThreadPool::loadDeviceInfo()
 
 void ThreadPool::updateDeviceInfo()
 {
-    // 在线程池执行之前，先用单线程获取hwinfo信息，因为在线程池里面执行hwinfo命令，可能出现死机的问题
-    qint64 begin = QDateTime::currentMSecsSinceEpoch();
-    runHwinfoCmd();
-    qint64 end = QDateTime::currentMSecsSinceEpoch();
-    qInfo() << "updateDeviceInfo*******************************" << (end - begin) / 1000;
-
     // 根据m_ListCmd生成所有设备信息
     QObjectCleanupHandler *cleaner = new QObjectCleanupHandler;
     cleaner->setParent(this);
@@ -54,14 +41,6 @@ void ThreadPool::updateDeviceInfo()
         cleaner->add(task);
         start(task);
         task->setAutoDelete(true);
-    }
-}
-
-void ThreadPool::runHwinfoCmd()
-{
-    QList<Cmd>::iterator itHw = m_ListHwinfo.begin();
-    for (; itHw != m_ListHwinfo.end(); ++itHw) {
-        runCmdToCache(*itHw);
     }
 }
 
@@ -84,28 +63,6 @@ void ThreadPool::runCmdToCache(const Cmd &cmd)
 
 void ThreadPool::initCmd()
 {
-    // According to Huawei's requirements , Modify the way of judging klu and panguv
-    // 获取华为KLU、PanguV信息
-//    Cmd cmdDbus;
-//    cmdDbus.cmd = QString("%1 %2%3").arg("gdbus introspect -y -d com.deepin.system.SystemInfo -o /com/deepin/system/SystemInfo -p >").arg(PATH).arg("gdbus.txt");
-//    cmdDbus.file = "gdbus.txt";
-//    cmdDbus.canNotReplace = false;
-//    m_ListCmd.append(cmdDbus);
-
-//    // 获取架构信息
-//    Cmd cmdUname;
-//    cmdUname.cmd = QString("%1 %2%3").arg("uname -m > ").arg(PATH).arg("uname_m.txt");
-//    cmdUname.file = "uname_m.txt";
-//    cmdUname.canNotReplace = false;
-//    m_ListCmd.append(cmdUname);
-    // 添加hwinfo --usb命令
-//    Cmd cmdHwinfoUsb;
-//    cmdHwinfoUsb.cmd = QString("%1 %2%3").arg("hwinfo --usb > ").arg(PATH).arg("hwinfo_usb.txt");
-//    cmdHwinfoUsb.file = "hwinfo_usb.txt";
-//    cmdHwinfoUsb.canNotReplace = false;
-//    m_ListHwinfo.append(cmdHwinfoUsb);
-//    m_ListUpdate.append(cmdHwinfoUsb);
-
     // 添加lshw命令
     Cmd cmdLshw;
     cmdLshw.cmd = QString("%1 %2%3").arg("lshw > ").arg(PATH).arg("lshw.txt");
@@ -169,55 +126,6 @@ void ThreadPool::initCmd()
     cmdDmi17.file = "dmidecode_17.txt";
     cmdDmi17.canNotReplace = true;
     m_ListCmd.append(cmdDmi17);
-
-    // 添加hwinfo --sound命令
-    Cmd cmdHwinfoSound;
-    cmdHwinfoSound.cmd = QString("%1 %2%3").arg("hwinfo --sound > ").arg(PATH).arg("hwinfo_sound.txt");
-    cmdHwinfoSound.file = "hwinfo_sound.txt";
-    cmdHwinfoSound.canNotReplace = false;
-//    m_ListHwinfo.append(cmdHwinfoSound);
-
-    // 添加hwinfo --network命令
-    Cmd cmdHwinfoNetwork;
-    cmdHwinfoNetwork.cmd = QString("%1 %2%3").arg("hwinfo --network > ").arg(PATH).arg("hwinfo_network.txt");
-    cmdHwinfoNetwork.file = "hwinfo_network.txt";
-    cmdHwinfoNetwork.canNotReplace = false;
-//    m_ListHwinfo.append(cmdHwinfoNetwork);
-
-    // 添加hwinfo --keyboard命令
-    Cmd cmdHwinfoKeyboard;
-    cmdHwinfoKeyboard.cmd = QString("%1 %2%3").arg("hwinfo --keyboard > ").arg(PATH).arg("hwinfo_keyboard.txt");
-    cmdHwinfoKeyboard.file = "hwinfo_keyboard.txt";
-    cmdHwinfoKeyboard.canNotReplace = false;
-//    m_ListHwinfo.append(cmdHwinfoKeyboard);
-
-    // 添加hwinfo --network命令
-    Cmd cmdHwinfoCdrom;
-    cmdHwinfoCdrom.cmd = QString("%1 %2%3").arg("hwinfo --cdrom > ").arg(PATH).arg("hwinfo_cdrom.txt");
-    cmdHwinfoCdrom.file = "hwinfo_cdrom.txt";
-    cmdHwinfoCdrom.canNotReplace = false;
-//    m_ListHwinfo.append(cmdHwinfoCdrom);
-
-    // 添加hwinfo --disk命令
-    Cmd cmdHwinfoDisk;
-    cmdHwinfoDisk.cmd = QString("%1 %2%3").arg("hwinfo --disk > ").arg(PATH).arg("hwinfo_disk.txt");
-    cmdHwinfoDisk.file = "hwinfo_disk.txt";
-    cmdHwinfoDisk.canNotReplace = false;
-//    m_ListHwinfo.append(cmdHwinfoDisk);
-
-    // 添加hwinfo --display命令
-    Cmd cmdHwinfoDisplay;
-    cmdHwinfoDisplay.cmd = QString("%1 %2%3").arg("hwinfo --display > ").arg(PATH).arg("hwinfo_display.txt");
-    cmdHwinfoDisplay.file = "hwinfo_display.txt";
-    cmdHwinfoDisplay.canNotReplace = true;
-//    m_ListHwinfo.append(cmdHwinfoDisplay);
-
-    // 添加hwinfo --mouse命令
-    Cmd cmdHwinfoMouse;
-    cmdHwinfoMouse.cmd = QString("%1 %2%3").arg("hwinfo --mouse > ").arg(PATH).arg("hwinfo_mouse.txt");
-    cmdHwinfoMouse.file = "hwinfo_mouse.txt";
-    cmdHwinfoMouse.canNotReplace = false;
-//    m_ListHwinfo.append(cmdHwinfoMouse);
 
     // 添加hwinfo --power命令
     Cmd cmdUpower;
@@ -305,5 +213,4 @@ void ThreadPool::initCmd()
     cmdHwinfo.canNotReplace = false;
     m_ListCmd.append(cmdHwinfo);
     m_ListUpdate.append(cmdHwinfo);
-//    m_ListHwinfo.append(cmdHwinfo);
 }
